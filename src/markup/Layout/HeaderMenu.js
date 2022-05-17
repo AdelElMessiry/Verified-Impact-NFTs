b525fa3b0f37b55e18694547366b6b6925016a4c
Ebrahim Hamdy <ebrahim0hamdy@gmail.com>
2022-05-17 19:44:18 +0300

    fix: get token details

--- src/markup/Layout/HeaderMenu.js
+++ src/markup/Layout/HeaderMenu.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { getBeneficiariesCampaignsList } from '../../api/beneficiaryInfo';
 import { getCreatorsCollectionsList } from '../../api/creatorInfo';
+import { getNFTsList } from '../../api/nftInfo';
 import { useAuth } from '../../contexts/AuthContext';
 
 const HeaderMenu = () => {
@@ -12,8 +13,7 @@ const HeaderMenu = () => {
   const [creators, setCreators] = useState();
   useEffect(() => {
     (async () => {
-      let beneficiaryList =
-        !beneficiaries && (await getBeneficiariesCampaignsList());
+      let beneficiaryList = !beneficiaries && (await getNFTsList());
       !beneficiaries && setBeneficiaries(beneficiaryList);
       let creatorsList = !creators && (await getCreatorsCollectionsList());
       !creators && setCreators(creatorsList);
