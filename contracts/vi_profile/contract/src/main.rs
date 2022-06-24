9c92de73e5f95740a5a3c2cd1e022fdf6b00f1cf
Ebrahim Hamdy <ebrahim0hamdy@gmail.com>
2022-06-24 18:57:16 +0300

    fix: call contract profile from NFT [WIP]

--- contracts/vi_profile/contract/src/main.rs
+++ contracts/vi_profile/contract/src/main.rs
@@ -1,15 +1,21 @@
-#![no_main]
 #![no_std]
-#[macro_use]
+#![no_main]
+
+#[cfg(not(target_arch = "wasm32"))]
+compile_error!("target arch should be wasm32: compile with '--target wasm32-unknown-unknown'");
+
+// We need to explicitly import the std alloc crate and `alloc::string::String` as we're in a
+// `no_std` environment.
 extern crate alloc;
 
 use alloc::{
     boxed::Box,
     collections::{BTreeMap, BTreeSet},
+    format,
     string::{String, ToString},
+    vec,
     vec::Vec,
 };
-
 use casper_contract::{
     contract_api::{
         runtime::{self, revert},
@@ -17,12 +23,11 @@ use casper_contract::{
     },
     unwrap_or_revert::UnwrapOrRevert,
 };
-
 use casper_types::{
-    runtime_args, ApiError, CLType, CLTyped, CLValue, ContractPackageHash, EntryPoint,
-    EntryPointAccess, EntryPointType, EntryPoints, Group, Key, Parameter, RuntimeArgs, URef, U256,
+    contracts::{EntryPoint, EntryPointAccess, EntryPointType, EntryPoints},
+    runtime_args, ApiError, CLType, CLTyped, CLValue, ContractPackageHash, Group, Key, Parameter,
+    RuntimeArgs, URef, U256,
 };
-
 use contract_utils::{AdminControl, ContractContext, OnChainContractStorage};
 
 #[repr(u16)]
@@ -45,14 +50,13 @@ pub type Profile = BTreeMap<String, String>;
 #[derive(Default)]
 struct ViProfile(OnChainContractStorage);
 
+impl ProfileControl<OnChainContractStorage> for ViProfile {}
+impl AdminControl<OnChainContractStorage> for ViProfile {}
 impl ContractContext<OnChainContractStorage> for ViProfile {
     fn storage(&self) -> &OnChainContractStorage {
         &self.0
     }
 }
-
-impl ProfileControl<OnChainContractStorage> for ViProfile {}
-impl AdminControl<OnChainContractStorage> for ViProfile {}
 
 impl ViProfile {
     fn constructor(&mut self) {
@@ -99,12 +103,6 @@ impl ViProfile {
         mail: String,
         profile_type: String,
     ) -> Result<(), Error> {
-        // let caller = ViProfile::default().get_caller();
-
-        // if !ViProfile::default().is_admin(caller) {
-        //     revert(ApiError::User(20));
-        // }
-
         match mode.as_str() {
             "ADD" | "UPDATE" => {
                 let cloned_mode = mode.clone();
@@ -180,10 +178,6 @@ impl ViProfile {
         mail: String,
         profile_type: String,
     ) -> Result<(), Error> {
-        // let caller = ViProfile::default().get_caller();
-        // if !ViProfile::default().is_admin(caller) {
-        //     revert(ApiError::User(20));
-        // }
         self.set_profile(
             mode,
             address,
@@ -247,7 +241,16 @@ fn is_profile_exist() {
 }
 
 #[no_mangle]
-pub extern "C" fn add_profile() {
+fn remove_profile() {
+    let address = runtime::get_named_arg::<Key>("address");
+
+    ViProfile::default()
+        .delete_profile(address)
+        .unwrap_or_revert();
+}
+
+#[no_mangle]
+pub extern "C" fn add_profile() -> () {
     let mode = runtime::get_named_arg::<String>("mode");
     let address = runtime::get_named_arg::<Key>("address");
     let username = runtime::get_named_arg::<String>("username");
@@ -292,74 +295,50 @@ pub extern "C" fn add_profile() {
 }
 
 #[no_mangle]
-fn remove_profile() {
-    let address = runtime::get_named_arg::<Key>("address");
-
-    ViProfile::default()
-        .delete_profile(address)
-        .unwrap_or_revert();
-}
-
-#[no_mangle]
 pub extern "C" fn call() {
-    let admin: Key = runtime::get_named_arg("admin");
-    let contract_name: String = runtime::get_named_arg("contract_name");
-    let (contract_hash, contract_version) = storage::new_contract(
+    let (contract_package_hash, _) = storage::create_contract_package_at_hash();
+    let (contract_hash, _) = storage::add_contract_version(
+        contract_package_hash,
         get_entry_points(),
-        None,
-        Some(String::from(&format!(
-            "{}_contract_package_hash",
-            contract_name
-        ))),
-        Some(format!("{}_package_access_token", contract_name)),
+        Default::default(),
     );
-    let version_uref = storage::new_uref(contract_version);
+    // let contract_name: String = "profile".to_string();
+    let admin: Key = runtime::get_named_arg("admin");
 
     let constructor_args = runtime_args! {
         "admin" => admin
     };
 
-    let package_hash = ContractPackageHash::new(
-        runtime::get_key(&format!("{}_contract_package_hash", contract_name))
-            .unwrap_or_revert()
-            .into_hash()
-            .unwrap_or_revert(),
-    );
+    // let package_hash = ContractPackageHash::new(
+    //     runtime::get_key(&format!("{}_contract_package_hash", contract_name))
+    //         .unwrap_or_revert()
+    //         .into_hash()
+    //         .unwrap_or_revert(),
+    // );
 
-    // Add the constructor group to the package hash with a single URef.
-    let constructor_access: URef =
-        storage::create_contract_user_group(package_hash, "constructor", 1, Default::default())
-            .unwrap_or_revert()
-            .pop()
-            .unwrap_or_revert();
+    // // Add the constructor group to the package hash with a single URef.
+    // let constructor_access: URef =
+    //     storage::create_contract_user_group(package_hash, "constructor", 1, Default::default())
+    //         .unwrap_or_revert()
+    //         .pop()
+    //         .unwrap_or_revert();
 
     // Call the constructor entry point
     let _: () = runtime::call_contract(contract_hash, "constructor", constructor_args);
 
-    // Remove all URefs from the constructor group, so no one can call it for the second time.
-    let mut urefs = BTreeSet::new();
-    urefs.insert(constructor_access);
+    // // Remove all URefs from the constructor group, so no one can call it for the second time.
+    // let mut urefs = BTreeSet::new();
+    // urefs.insert(constructor_access);
 
-    storage::remove_contract_user_group_urefs(package_hash, "constructor", urefs)
-        .unwrap_or_revert();
+    // storage::remove_contract_user_group_urefs(package_hash, "constructor", urefs)
+    //     .unwrap_or_revert();
 
-    // Store contract in the account's named keys.
+    runtime::put_key("profile_contract_hash", contract_hash.into());
+    let contract_hash_pack = storage::new_uref(contract_hash);
+    runtime::put_key("profile_contract_hash_wrapped", contract_hash_pack.into());
     runtime::put_key(
-        &format!("{}_contract_hash", contract_name),
-        contract_hash.into(),
-    );
-    runtime::put_key(
-        &format!("{}_contract_hash_wrapped", contract_name),
-        storage::new_uref(contract_hash).into(),
-    );
-    runtime::put_key(
-        &format!("{}_package_hash_wrapped", contract_name),
-        storage::new_uref(package_hash).into(),
-    );
-
-    runtime::put_key(
-        &format!("{}_version_wrapped", contract_name),
-        version_uref.into(),
+        "profile_contract_package_hash",
+        contract_package_hash.into(),
     );
 }
 
