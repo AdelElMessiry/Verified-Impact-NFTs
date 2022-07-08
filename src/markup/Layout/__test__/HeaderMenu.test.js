dcfd7fc2e0c6fcfa50a5441994b2927339679750
Abla Ibraheem <zozy_yosef@hotmail.com>
2022-07-08 02:08:33 +0200

    fix header test case

--- src/markup/Layout/__test__/HeaderMenu.test.js
+++ src/markup/Layout/__test__/HeaderMenu.test.js
@@ -1,9 +1,8 @@
 import React from "react";
 import {render}  from "react-dom";
 import {BrowserRouter}  from "react-router-dom";
-import Header1 from "../Header1";
-
+import HeaderMenu from "../HeaderMenu";
 it("renders without crashing",()=>{
     const div=document.createElement("div");
-    render(<BrowserRouter><Header1/></BrowserRouter>,div)
+    render(<BrowserRouter><HeaderMenu/></BrowserRouter>,div)
 })
