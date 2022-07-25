95912f8ad83c58c3a4e2cd828cc8dea77e6bcbe0
Toqa Hagag <toqa.hagag12@gmail.com>
2022-07-26 01:45:10 +0200

    fix calling after last changes

--- src/markup/Pages/Admin/MintNFT.js
+++ src/markup/Pages/Admin/MintNFT.js
@@ -124,7 +124,7 @@ const MintNFT = () => {
         campaigns.filter(
           ({ wallet_address }) => (savedData?savedData.beneficiary:beneficiaries?.filter(
             ({ approved }) => approved === 'true'
-          )[0]?.address) === wallet_address
+          )[0]?.address) === wallet_address.slice(13).replace(")","")
         )
       );
     !campaignsList && campaigns?.length && setAllCampaignsList(campaigns);
