6947935967dcbaf597a62b3986e20a75d8e98482
Abla Ibraheem <zozy_yosef@hotmail.com>
2022-09-15 17:35:24 +0200

    modify twitter text

--- src/markup/Element/TwitterShare/CampaignOrCollectionTwitterShare.js
+++ src/markup/Element/TwitterShare/CampaignOrCollectionTwitterShare.js
@@ -20,10 +20,10 @@ const CampaignOrCollectionTwitterShare = ({
       }
       title={
         creator === ''
-          ? `I support the "${campaign}" #NFT campaign, ${beneficiaryPercentage}% of the proceeds go to the "${beneficiary}". check it out \n @vinfts @Casper_Network @DEVXDAO`
+          ? `I support the "${campaign}" #NFT campaign, ${beneficiaryPercentage}% of the proceeds go to the "${beneficiary}". check it out \n @Casper_Network @DEVXDAO`
           : `I support the "${collection}" #NFT collection, ${beneficiaryPercentage}% of the proceeds go to the "${beneficiary}" and ${
               100 - Number(beneficiaryPercentage)
-            }% go to"${creator}". check it out\n@vinfts @Casper_Network @DEVXDAO`
+            }% go to"${creator}". check it out\n @Casper_Network @DEVXDAO`
       }
     >
       <TwitterIcon
