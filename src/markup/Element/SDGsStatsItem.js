ae34ce055ef882c784c7d7c537f0b55b4aa5ad69
Toqa Hagag <toqa.hagag12@gmail.com>
2022-09-16 19:06:20 +0200

    add sdg tooltip

--- src/markup/Element/SDGsStatsItem.js
+++ src/markup/Element/SDGsStatsItem.js
@@ -1,35 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Spinner } from 'react-bootstrap';
+import VINftsTooltip from './Tooltip';
 
 const SDGsStatsItem = ({ data }) => {
   return (
-    <Link
-    to={`./SDGGoalNFTs?id=${data.value}`}
-    className='dez-page text-white'
-  >
-    <div className="sdgs-goals-card">
-      <img
-        src={process.env.PUBLIC_URL + 'images/sdgsGoals/' + data.image}
-        alt=""
-        className="img img-fluid fit-img fit-img-cover"
-      />
-      <div className="text-center">
-        {data.nftNumber !== undefined ? (
-          <>
-            <span className='text-success'>{data.nftNumber || 0}</span> 
-          </>
-        ) : (
-          <>
-            <Spinner
-              animation="border"
-              variant="success"
-              className="stats-spinner"
-            />
-          </>
-        )}<span className='ml-1 text-gray-dark'>NFTS</span>
-      </div>
-    </div>
+    <Link to={`./SDGGoalNFTs?id=${data.value}`} className="dez-page text-white">
+      <VINftsTooltip title={data.description}>
+        <div className="sdgs-goals-card">
+          <img
+            src={process.env.PUBLIC_URL + 'images/sdgsGoals/' + data.image}
+            alt=""
+            className="img img-fluid fit-img fit-img-cover"
+          />
+          <div className="text-center">
+            {data.nftNumber !== undefined ? (
+              <>
+                <span className="text-success">{data.nftNumber || 0}</span>
+              </>
+            ) : (
+              <>
+                <Spinner
+                  animation="border"
+                  variant="success"
+                  className="stats-spinner"
+                />
+              </>
+            )}
+            <span className="ml-1 text-gray-dark">NFTS</span>
+          </div>
+        </div>
+      </VINftsTooltip>
     </Link>
   );
 };
