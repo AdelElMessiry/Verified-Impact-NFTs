0f7bb863d42325494efa3e9975a5a684b0bd6d6f
Toqa Hagag <toqa.hagag12@gmail.com>
2022-09-19 21:37:00 +0200

    add sdgs data

--- src/markup/Element/AddEditCampaignForm.js
+++ src/markup/Element/AddEditCampaignForm.js
@@ -25,7 +25,7 @@ const AddEditCampaignForm = ({
   const [beneficiary, setBeneficiary] = React.useState();
   const [isButtonClicked, setIsButtonClicked] = React.useState(false);
   const [showURLErrorMsg, setShowURLErrorMsg] = React.useState(false);
-  const [SDGsGoals, setSDGsGoals] = React.useState([]);
+  const [SDGsGoals, setSDGsGoals] = React.useState(data ? data.sdgs_ids?.split(',') : []);
   const [SDGsGoalsData, setSDGsGoalsData] = React.useState([]);
 
   //getting beneficiary details
@@ -65,7 +65,7 @@ const AddEditCampaignForm = ({
       requestedRoyalty: data ? data.requested_royalty : '',
     },
   });
-setSDGsGoals(data ? data.sdgs_ids?.split(',') : [])
+
   const checkURLValidation = (value) => {
     if (validator.isURL(value)) {
       setShowURLErrorMsg(false);
