1d4a8719cb4ca2e8a1dbc748e2d84d117d61526a
Ebrahim Hamdy <ebrahim0hamdy@gmail.com>
2022-09-21 19:54:28 +0300

    fix: ipfs url

--- src/utils/contract-utils.ts
+++ src/utils/contract-utils.ts
@@ -260,7 +260,9 @@ export const transferFees = async (buyer: string, tokenId: string) => {
 };
 
 export const getNFTImage = async (tokenMetaUri: string) => {
-  const resp = await fetch('https://vinfts.mypinata.cloud/' + tokenMetaUri);
+  const resp = await fetch(
+    'https://vinfts.mypinata.cloud/ipfs/' + tokenMetaUri
+  );
   const imgString = await resp.text();
 
   return imgString;
