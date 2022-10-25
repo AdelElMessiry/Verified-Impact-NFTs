97621d2899dbebd47ccbcafb44e2063e34fb8ab3
Abla Ibraheem <zozy_yosef@hotmail.com>
2022-10-25 03:33:42 +0200

    change marketplace layout

--- src/markup/Pages/BeneficiariesMarket.js
+++ src/markup/Pages/BeneficiariesMarket.js
@@ -1,95 +1,102 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Col, Container, Row, Spinner } from 'react-bootstrap';
-import { useAuth } from '../../contexts/AuthContext'
+import { Col, Row, Spinner } from 'react-bootstrap';
+import Carousel from 'react-elastic-carousel';
+import SimpleReactLightbox from 'simple-react-lightbox';
+import { SRLWrapper } from 'simple-react-lightbox';
 import { useNFTState } from '../../contexts/NFTContext';
 import Layout from '../Layout';
-import PromptLogin from './PromptLogin';
 import bnr1 from './../../images/banner/bnr1.jpg';
-import MarketPlaceSingleRow from '../Element/marketPlaceSingleRow';
+import UserCard from '../Element/userCard';
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 992, itemsToShow: 4 },
+  { width: 1200, itemsToShow: 4 },
+];
 
- const BeneficiariesMarket = () => {
-  const { isLoggedIn } = useAuth();
+const options = {
+  buttons: { showDownloadButton: false },
+};
+const BeneficiariesMarket = () => {
   const { beneficiaries } = useNFTState();
   return (
     <Layout>
-    <div className='page-content bg-white'>
-      {/* <!-- inner page banner --> */}
-      <div
-        className='dlab-bnr-inr overlay-primary bg-pt'
-        style={{ backgroundImage: 'url(' + bnr1 + ')' }}
-      >
-        <div className='container'>
-          <div className='dlab-bnr-inr-entry'>
-            <h1 className='text-white d-flex align-items-center'>
-              <span className='mr-1'>
-                Discover Beneficiaries{' '}
-              </span>
-            </h1>
+      <div className='page-content bg-white'>
+        {/* <!-- inner page banner --> */}
+        <div
+          className='dlab-bnr-inr overlay-primary bg-pt'
+          style={{ backgroundImage: 'url(' + bnr1 + ')' }}
+        >
+          <div className='container'>
+            <div className='dlab-bnr-inr-entry'>
+              <h1 className='text-white d-flex align-items-center'>
+                <span className='mr-1'>
+                  Discover Beneficiaries{' '}
+                </span>
+              </h1>
 
-            <div className='breadcrumb-row'>
-              <ul className='list-inline'>
-                <li>
-                  <Link to={'#'}>Home</Link>
-                </li>
-                <li className='ml-1'>Discover Beneficiaries</li>
-              </ul>
+              <div className='breadcrumb-row'>
+                <ul className='list-inline'>
+                  <li>
+                    <Link to={'#'}>Home</Link>
+                  </li>
+                  <li className='ml-1'>Discover Beneficiaries</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* <!-- inner page banner END --> */}
-      {/* <!-- contact area --> */}      
+        {/* <!-- inner page banner END --> */}
+        {/* <!-- contact area --> */}
         <div className='section-full content-inner shop-account'>
           {/* <!-- Product --> */}
-          <div className='container'>
+          <div>
             <div>
               <div className=' m-auto m-b30'>
-                <Container>
-                  <Row>
-                    <Col>
-                      {beneficiaries  ? (
-                        <table className='table'>
-                          <thead>
-                            <tr>
-                              <th scope='col'></th>
-                              <th scope='col'>Name</th>
-                              <th scope='col'>Address</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {beneficiaries.length > 0 ? (
-                              beneficiaries?.map((beneficiary) => (
-                                beneficiary.isApproved == "true" &&
-                                <MarketPlaceSingleRow
-                                  item={beneficiary}
-                                  key={beneficiary.address}
-                                />
-                              ))
-                            ) : (
-                              <h4 className='text-muted text-center my-5'>
-                                No Beneficiaries registered yet
-                              </h4>
-                            )}
-                          </tbody>
-                        </table>
-                      ) : (
-                        <div className='vinft-section-loader text-center my-5'>
-                          <Spinner animation='border' variant='success' />
-                          <p>Fetching Beneficiaries Please wait...</p>
-                        </div>
-                      )}
-                    </Col>
-                  </Row>
-                </Container>
+                <Row>
+                  <Col>
+                    {beneficiaries ? (
+                      <SimpleReactLightbox>
+                        <SRLWrapper options={options}>
+                          <div className='clearfix portfolio nfts-slider'>
+                            <ul
+                              id='masonry'
+                              className='dlab-gallery-listing gallery-grid-4 gallery mfp-gallery port-style1'
+                            >
+                              {beneficiaries &&
+                                <Carousel itemsToShow={3} breakPoints={breakPoints}>
+                                  {beneficiaries.filter((beneficiary) => beneficiary.isApproved == "true").map((item, index) => (
+                                    <React.Fragment key={`${index}${item.address}`}>
+                                      <li className='web design card-container p-a0'>
+                                        <UserCard item={item} />
+                                      </li>
+                                    </React.Fragment>
+                                  ))}
+                                </Carousel>
+                              }
+                            </ul>
+                          </div>
+                        </SRLWrapper>
+                      </SimpleReactLightbox>
+                    ) : (
+                      <div className='vinft-section-loader text-center my-5'>
+                        <Spinner animation='border' variant='success' />
+                        <p>Fetching Beneficiaries Please wait...</p>
+                      </div>
+                    )}
+                  </Col>
+                </Row>
               </div>
+
             </div>
           </div>
           {/* <!-- Product END --> */}
         </div>
-      {/* <!-- contact area  END --> */}
-    </div>
-  </Layout>
+        {/* <!-- contact area  END --> */}
+      </div>
+    </Layout>
   )
 }
 export default BeneficiariesMarket
--- src/markup/Pages/CreatorsMarketPlace.js
+++ src/markup/Pages/CreatorsMarketPlace.js
@@ -1,121 +1,120 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Col, Container, Row, Spinner } from 'react-bootstrap';
-import { useAuth } from '../../contexts/AuthContext'
+import { Col, Row, Spinner } from 'react-bootstrap';
+import Carousel from 'react-elastic-carousel';
+import SimpleReactLightbox from 'simple-react-lightbox';
+import { SRLWrapper } from 'simple-react-lightbox';
 import { useNFTState } from '../../contexts/NFTContext';
 import Layout from '../Layout';
-import PromptLogin from './PromptLogin';
 import bnr1 from './../../images/banner/bnr1.jpg';
-import MarketPlaceSingleRow from '../Element/marketPlaceSingleRow';
 import { profileClient } from '../../api/profileInfo';
- const CreatorsMarketPlace = () => {
-  const { isLoggedIn } = useAuth();
+import UserCard from '../Element/userCard';
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 992, itemsToShow: 4 },
+  { width: 1200, itemsToShow: 4 },
+];
+
+const options = {
+  buttons: { showDownloadButton: false },
+};
+const CreatorsMarketPlace = () => {
   const { creators } = useNFTState();
   const [profileDetails, setProfileDetails] = React.useState([]);
 
   const getProfile = React.useCallback(() => {
-    creators?.map(async(creator)=>{
+    creators?.map(async (creator) => {
       let userProfiles = await profileClient.getProfile(creator.address, true);
       if (userProfiles) {
         if (userProfiles.err === 'Address Not Found') {
           setProfileDetails(null);
         } else {
           let list = Object.values(userProfiles)[0];
-          //setMyArray(oldArray => [...oldArray, newElement]);
           userProfiles &&
             setProfileDetails(profileDetails => [...profileDetails, list.creator]);
         }
       }
     })
-    console.log(profileDetails , "profileDetails ");
   }, [creators]);
-  React.useEffect(()=>{
-    console.log(creators , "creators creators")
-   creators&& getProfile()
-  },[creators])
+  React.useEffect(() => {
+    creators && getProfile()
+  }, [creators])
   return (
     <Layout>
-    <div className='page-content bg-white'>
-      {/* <!-- inner page banner --> */}
-      <div
-        className='dlab-bnr-inr overlay-primary bg-pt'
-        style={{ backgroundImage: 'url(' + bnr1 + ')' }}
-      >
-        <div className='container'>
-          <div className='dlab-bnr-inr-entry'>
-            <h1 className='text-white d-flex align-items-center'>
-              <span className='mr-1'>
-                Discover Creators{' '}
-              </span>
-            </h1>
-
-            <div className='breadcrumb-row'>
-              <ul className='list-inline'>
-                <li>
-                  <Link to={'#'}>Home</Link>
-                </li>
-                <li className='ml-1'>Discover Creators</li>
-              </ul>
+      <div className='page-content bg-white'>
+        {/* <!-- inner page banner --> */}
+        <div
+          className='dlab-bnr-inr overlay-primary bg-pt'
+          style={{ backgroundImage: 'url(' + bnr1 + ')' }}
+        >
+          <div className='container'>
+            <div className='dlab-bnr-inr-entry'>
+              <h1 className='text-white d-flex align-items-center'>
+                <span className='mr-1'>
+                  Discover Creators{' '}
+                </span>
+              </h1>
+              <div className='breadcrumb-row'>
+                <ul className='list-inline'>
+                  <li>
+                    <Link to={'#'}>Home</Link>
+                  </li>
+                  <li className='ml-1'>Discover Creators</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* <!-- inner page banner END --> */}
-      {/* <!-- contact area --> */}
-      {!isLoggedIn ? (
-        <PromptLogin />
-      ) : (
+        {/* <!-- inner page banner END --> */}
+        {/* <!-- contact area --> */}
         <div className='section-full content-inner shop-account'>
           {/* <!-- Product --> */}
-          <div className='container'>
+          <div>
             <div>
               <div className=' m-auto m-b30'>
-                <Container>
-                  <Row>
-                    <Col>
-                      {profileDetails && creators ? (
-                        <table className='table'>
-                          <thead>
-                            <tr>
-                              <th scope='col'></th>
-                              <th scope='col'>Name</th>
-                              <th scope='col'>Address</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {profileDetails.length > 0 ? (
-                              profileDetails?.map((creator) => (
-                                <MarketPlaceSingleRow
-                                  type={"creators"}
-                                  item={creator}
-                                  key={creator.address}
-                                />
-                              ))
-                            ) : (
-                              <h4 className='text-muted text-center my-5'>
-                                No Creators registered yet
-                              </h4>
-                            )}
-                          </tbody>
-                        </table>
-                      ) : (
-                        <div className='vinft-section-loader text-center my-5'>
-                          <Spinner animation='border' variant='success' />
-                          <p>Fetching Creators Please wait...</p>
-                        </div>
-                      )}
-                    </Col>
-                  </Row>
-                </Container>
+                <Row>
+                  <Col>
+                    {profileDetails.length > 0 ? (
+                      <SimpleReactLightbox>
+                        <SRLWrapper options={options}>
+                          <div className='clearfix portfolio nfts-slider'>
+                            <ul
+                              id='masonry'
+                              className='dlab-gallery-listing gallery-grid-4 gallery mfp-gallery port-style1'
+                            >
+                              {profileDetails &&
+                                <Carousel itemsToShow={3} breakPoints={breakPoints}>
+                                  {profileDetails.map((item, index) => (
+                                    <React.Fragment key={`${index}${item.address}`}>
+                                      <li className='web design card-container p-a0'>
+                                        <UserCard item={item} type={"creators"} />
+                                      </li>
+                                    </React.Fragment>
+                                  ))}
+                                </Carousel>
+                              }
+                            </ul>
+                          </div>
+                        </SRLWrapper>
+                      </SimpleReactLightbox>
+                    ) : (
+                      <div className='vinft-section-loader text-center my-5'>
+                        <Spinner animation='border' variant='success' />
+                        <p>Fetching Creators Please wait...</p>
+                      </div>
+                    )}
+                  </Col>
+                </Row>
               </div>
             </div>
           </div>
           {/* <!-- Product END --> */}
         </div>
-      )}
-      {/* <!-- contact area  END --> */}
-    </div>
-  </Layout>
+        {/* <!-- contact area  END --> */}
+      </div>
+    </Layout>
   )
 }
 export default CreatorsMarketPlace
