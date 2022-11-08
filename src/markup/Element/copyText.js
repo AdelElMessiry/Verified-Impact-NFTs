6f5f46b82c3cedffb1d8e6ef34eb8438c99aca9c
Toqa Hagag <toqa.hagag12@gmail.com>
2022-11-08 15:47:05 +0200

    fix copy text style

--- src/markup/Element/copyText.js
+++ src/markup/Element/copyText.js
@@ -6,13 +6,13 @@ import copyIcon from './../../images/icon/copy.png';
 //shared tooltip component
 const CopyText = ({ link }) => {
   return (
-   <div onClick={(e) => {
+   <span onClick={(e) => {
        e.stopPropagation(); navigator.clipboard.writeText(link);VIToast.success('Text Copied successfully!');
       }}> <img
       src={copyIcon}
       width="40"
       className="cursor-pointer not-clickable-link"
-    /></div>
+    /></span>
   );
 };
 export default CopyText;
