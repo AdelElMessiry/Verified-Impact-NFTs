9ed38646baa6573e67500d774b6c70c3e16c7343
Abla Ibraheem <zozy_yosef@hotmail.com>
2022-11-28 22:13:58 +0200

    signup as a beneficiary add beneficiary/campaign/collectionn

--- src/markup/Pages/PromptLogin.js
+++ src/markup/Pages/PromptLogin.js
@@ -1,26 +1,20 @@
-import { useAuth } from '../../contexts/AuthContext'
+import { useAuth } from '../../contexts/AuthContext';
 
-// display in case the user makes a request required the user to login
-/**
- * display in case the user makes a request required the user to login
- *
- * @type {React.FC<Props>}
- * @returns {React.ReactElement} UI page
- */
+//returned when the page that user requested require login with casper and user not signing
 const PromptLogin = () => {
   const { login } = useAuth();
   return (
-    <div className='p-3 mt-5' style={{height:"100vh"}}>
+    <div className='p-3 mt-5' style={{ height: '100vh' }}>
       <h4 align='center' variant='h4'>
         Please connect to signer
-        <br/><br/>
-        <button onClick={login} className="btn btn-success">
+        <br />
+        <br />
+        <button onClick={login} className='btn btn-success'>
           Connect
         </button>
       </h4>
-
     </div>
-  )
-}
+  );
+};
 
-export default PromptLogin
+export default PromptLogin;
