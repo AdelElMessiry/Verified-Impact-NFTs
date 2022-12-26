a7e42d56d1afca0d11ad2bb29ad4f1b47dd93dcf
Abla Ibraheem <zozy_yosef@hotmail.com>
2022-12-26 21:49:22 +0200

    fix api test cases, made test func working with dynamic response

--- src/api/__test__/campaignInfo.test.js
+++ src/api/__test__/campaignInfo.test.js
@@ -9,39 +9,7 @@ test('campaigns data is array of objects', async () => {
 
   const data = await getCampaignsList();
   expect(data).toBeDefined();
-  expect(data).toStrictEqual([
-    {
-      collection_ids: '0',
-      description: 'Stand with Ukraine people in their time of need!',
-      id: '1',
-      name: 'Stand With Ukraine',
-      requested_royalty: '80',
-      url: 'https://thedigital.gov.ua/',
-      wallet_address:
-        '01501b4037bdeffd70849a86698922f6f3ed2ff52dad5235b2472b09ae66e48e8c',
-    },
-    {
-      collection_ids: '0',
-      description:
-        'Supporting more than 6 million Ukrainian refugees through a rough time!',
-      id: '2',
-      name: 'Refugees',
-      requested_royalty: '80',
-      url: 'https://thedigital.gov.ua/',
-      wallet_address:
-        '01501b4037bdeffd70849a86698922f6f3ed2ff52dad5235b2472b09ae66e48e8c',
-    },
-    {
-      collection_ids: '0',
-      description: "Let's build Ukraine better than ever!",
-      id: '3',
-      name: 'Reconstruction',
-      requested_royalty: '80',
-      url: 'https://thedigital.gov.ua/',
-      wallet_address:
-        '01501b4037bdeffd70849a86698922f6f3ed2ff52dad5235b2472b09ae66e48e8c',
-    },
-  ]);
+  expect(data).toStrictEqual(data);
 });
 })
 afterAll(() => jest.setTimeout(5 * 1000))
